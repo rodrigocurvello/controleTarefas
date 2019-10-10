a4a71a66785ab230f893869b138e63673aa46616
Cristhian <cristhian.heck@ifc.edu.br>
2019-10-10 17:09:08 -0300

    Alteração de tarefas e correções diversas
    Foi implementado método de alterar tarefas.
    Foi feito algumas correções na inserção e delete. Algumas outras funções criadas para limpar o código.

--- scripts.js
+++ scripts.js
@@ -17,26 +17,39 @@ if(localStorage.getItem("controleCodigo") == null){
 
 
 /**
- * Código para salvr no localStorage
+ * Código para salvar ou atualizar no localStorage 
  */
 
 function salvar(){ 
-    var novoCodigo = localStorage.getItem("controleCodigo");
-    //console.log(`1 ${localStorage.getItem("controleCodigo")}`);
-    var vetorCentral= JSON.parse(localStorage.getItem("vetorTarefas"));
-    var objetoJSON = {codigo:novoCodigo,descricao:`${$('#descricao').val()}`,cor:`${$('#cor').val()}`,estado:$('#estado').val(),data:`${$('#data').val()}`};
-    vetorCentral.push(objetoJSON);
-    localStorage.setItem("vetorTarefas",JSON.stringify(vetorCentral));
-    localStorage.setItem("controleCodigo", ++novoCodigo);
+    vetorCentral = retornaVetorCentral();
+    if($('#codigo').val() == -1){
+        var novoCodigo = localStorage.getItem("controleCodigo");
+        //console.log(`1 ${localStorage.getItem("controleCodigo")}`);
+        var objetoJSON = {codigo:novoCodigo,descricao:`${$('#descricao').val()}`,cor:`${$('#cor').val()}`,estado:$('#estado').val(),data:`${$('#data').val()}`};
+        vetorCentral.push(objetoJSON);    
+        localStorage.setItem("controleCodigo", ++novoCodigo);
+    } else {
+        var posicao = busca($('#codigo').val());
+        vetorCentral[posicao] = {codigo:$('#codigo').val(),descricao:`${$('#descricao').val()}`,cor:`${$('#cor').val()}`,estado:$('#estado').val(),data:`${$('#data').val()}`};
+        zeraCampos();
+    }
+    atualizaVetorCentral(vetorCentral);
     //console.log(`2 ${localStorage.getItem("controleCodigo")}`);
-    apresenta();
+    apresenta(vetorCentral);
+}
+function zeraCampos(){
+    $('#codigo').val(-1);
+    $('#descricao').val("");
+    $('#cor').val("#fff");
+    $('#estado').val(1);
+    $('#data').val("");
 }
 
 /**
- * Atualização da tabela (em andandamento)
+ * Atualização da tabela 
  */
-function apresenta(){
-    vetorCentral = JSON.parse(localStorage.getItem("vetorTarefas"));
+function apresenta(vetorCentral){
+   // vetorCentral = retornaVetorCentral();
     $('#tabelaTarefas').find("tr:gt(0)").remove();
     if(vetorCentral.length == 0){
         $('#tabelaTarefas').append("<tr><td  align='center' colspan='6'>Nenhuma tarefa cadastrada</td></tr>");
@@ -47,7 +60,7 @@ function apresenta(){
             <td>${vetor['descricao']}</td>  
             <td>${vetor['estado'] == 1 ? "Fazer" : vetor['estado'] == 2 ? "Fazendo" : "Finalizado"}</td>  
             <td bgcolor='${vetor['cor']}'>&nbsp;</td>
-            <td align="center"><a href="cad_marca.php?acao=editar&codigo=1"><img src="img/glyphicons-31-pencil.png" width="20em"></a></td>
+            <td align="center"><a href="javascript:altera(${vetor['codigo']})"><img src="img/glyphicons-31-pencil.png" width="20em"></a></td>
             <td align="center"><a href="javascript:exclui(${vetor['codigo']})"><img src="img/glyphicons-17-bin.png" width="17em"></a></td>          `;
             $('#tabelaTarefas').append(linha);
         }
@@ -57,19 +70,57 @@ function apresenta(){
 /**
  * Função para atualizar tarefa
  */
-
+function altera(id){
+    vetorCentral = retornaVetorCentral();
+    var posicao = busca(id);
+    var vetor = vetorCentral[posicao];
+    $('#codigo').val(id);
+    $('#descricao').val(vetor['descricao']);
+    $('#cor').val(vetor['cor']);
+    $('#estado').val(vetor['estado']);
+    $('#data').val(vetor['data']);
+}
 
  /**
   * Função para excluir tarefa
   */
  function exclui(id){
-    vetorCentral = JSON.parse(localStorage.getItem("vetorTarefas"));
+    vetorCentral = retornaVetorCentral();
+    var posicao = busca(id);
+    vetorCentral.splice(posicao, 1);  
+    atualizaVetorCentral(vetorCentral);
+    apresenta(vetorCentral);
+ }
+
+ /**
+  * Função que busca tarefa
+  */
+ function busca(id){
+    vetorCentral = retornaVetorCentral();
+    var retorno = -1;
     for(var i = 0; i < vetorCentral.length; i++){
         var vetor = vetorCentral[i];
         if(vetor['codigo'] == id){
-            vetorCentral.splice(i, 1);  
-        }
+            retorno = i;
+        } 
     }
-    localStorage.setItem("vetorTarefas",JSON.stringify(vetorCentral));
-    apresenta();
+    return retorno;
+ }
+
+
+ /**
+  * Funções que retorna ou atualiza o vetorCentral
+  */
+ function retornaVetorCentral(){
+     return JSON.parse(localStorage.getItem("vetorTarefas"));
+ }
+ function atualizaVetorCentral(valor){
+    localStorage.setItem("vetorTarefas",JSON.stringify(valor));
+ }
+
+ /**
+  * Função inicia
+  */
+ function inicia(){
+    apresenta(retornaVetorCentral());
  }
